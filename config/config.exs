74b824f5ed26e01ca1fab3ef3949b59c55e38c7b
Cody Brunner <me@codybrunner.com>
2022-01-16 10:49:38 -0500

    chore(config): modifies defaults of Ecto.Migration

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,10 @@
 import Config
 
+config :ectogram, Ectogram.Repo,
+  migration_foreign_key: [column: :id, type: :binary_id],
+  migration_primary_key: [name: :id, type: :binary_id],
+  migration_timestamps: [inserted_at: :created_at, type: :utc_datetime_usec, updated_at: :modified_at]
+
 config :ectogram,
   ecto_repos: [Ectogram.Repo]
 
