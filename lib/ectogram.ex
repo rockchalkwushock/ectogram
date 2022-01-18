ee126c434fd1174b71fef6a887bdcb8de9ad0dfd
Cody Brunner <me@codybrunner.com>
2022-01-17 19:22:22 -0500

    feat(ectogram): adds contexts for posts

--- lib/ectogram.ex
+++ lib/ectogram.ex
@@ -2,7 +2,8 @@ defmodule Ectogram do
   @moduledoc """
   Documentation for `Ectogram`.
   """
-  alias Ectogram.{Repo,User}
+  import Ecto.Query, warn: false
+  alias Ectogram.{Post,Repo,User}
 
   #############################
   # User
@@ -27,6 +28,36 @@ defmodule Ectogram do
     |> User.registration_changeset(attrs)
     |> Repo.insert()
   end
+
+  #############################
+  #############################
+
+  #############################
+  # Post
+  #############################
+
+  def get_post!(id), do: Repo.get!(Post, id)
+
+  def list_posts(), do: Repo.all(Post)
+
+  def list_posts_by_user!(%User{} = user) do
+    query = from p in Post, where: p.user_id == ^user.id
+    Repo.all(query)
+  end
+
+  def create_post(attrs \\ %{}) do
+    %Post{}
+    |> Post.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_post(%Post{} = post, attrs) do
+    post
+    |> Post.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_post(%Post{} = post), do: Repo.delete(post)
 
   #############################
   #############################
