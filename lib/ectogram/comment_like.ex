e503b85823e86b5c8f6153599ba05eef6c0a56f6
Cody Brunner <me@codybrunner.com>
2022-01-21 12:02:36 -0500

    feat(ectogram): adds schemas for CommentLike and PostLike

---
+++ lib/ectogram/comment_like.ex
@@ -0,0 +1,22 @@
+defmodule Ectogram.CommentLike do
+  use Ectogram.Schema
+  import Ecto.Changeset
+  alias Ectogram.{Comment,User}
+
+  @required_fields ~w(comment_id user_id)a
+
+  schema "comment_likes" do
+    belongs_to :comment, Comment
+    belongs_to :user, User
+
+    timestamps([modified_at: false])
+  end
+
+  def changeset(like, attrs) do
+    like
+    |> cast(attrs, @required_fields)
+    |> validate_required(@required_fields)
+    |> assoc_constraint(:comment)
+    |> assoc_constraint(:user)
+  end
+end
